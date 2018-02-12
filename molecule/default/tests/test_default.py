40de5b2fb84875f5bb9ef5456a4f4104706d7833
Malcolm Jones <malcolm@adobe.com>
2018-02-12 18:22:23 -0500

    chg: missing comma

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -9,7 +9,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
                          ['/home/vagrant/.bash_it/',
                           '/home/vagrant/.bash_it/completion/available/git.completion.bash',
                           '/home/vagrant/.bash_it/aliases/available/general.aliases.bash',
-                          '/home/vagrant/.bash_it/plugins/available/base.plugin.bash'
+                          '/home/vagrant/.bash_it/plugins/available/base.plugin.bash',
                           '/home/vagrant/.bash_it/plugins/available/history.plugin.bash'])
 def test_bash_it_folders(host, bash_it_files):
     f = host.file(bash_it_files)
